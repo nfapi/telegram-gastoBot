eaba8df7c6da05cfc554a5302f350274333929e5
Nicolas Fapitalle <nicolasfapitalle@gmail.com>
2026-02-11 17:46:45 -0300

    Refactor Telegram API token handling to use dynamic retrieval from Script Properties

--- telegramManager.js
+++ telegramManager.js
@@ -1,14 +1,23 @@
-const TELEGRAM_BOT_TOKEN = '7738124641:AAEDWAhTAzx4O9TdsoktsO8TrxHNe11kd3A';
-const TELEGRAM_API_URL = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN;
+function getTelegramToken(){
+  var token = PropertiesService.getScriptProperties().getProperty('TELEGRAM_BOT_TOKEN');
+  if (!token) {
+    throw new Error('TELEGRAM_BOT_TOKEN not set in Script Properties. Use setTelegramToken(token) or set it in Project Properties.');
+  }
+  return token;
+}
+
+function getTelegramApiUrl(){
+  return "https://api.telegram.org/bot" + getTelegramToken();
+}
 
 function getMe() {
-  var url = TELEGRAM_API_URL + "/getMe";
+  var url = getTelegramApiUrl() + "/getMe";
   var res = UrlFetchApp.fetch(url);
   Logger.log(res.getContentText());
 }
 
 function sendResponse(chatId, text) {
-  var url = TELEGRAM_API_URL + "/sendMessage?chat_id=" + chatId + "&text=" + encodeURIComponent(text);
+  var url = getTelegramApiUrl() + "/sendMessage?chat_id=" + chatId + "&text=" + encodeURIComponent(text);
   UrlFetchApp.fetch(url);
 }
 
@@ -16,8 +25,14 @@ function setWebhook() {
   // Función para configurar el webhook, solo necesitas ejecutarla una vez
   var webAppUrl = "https://script.google.com/macros/s/AKfycbzqMSl28redg_O8GKLzuLNjBoHmBFB5GLvc1Nqa5OLyHqHsAYaAvPYlhwp5v-4UrEzKUg/exec";
   Logger.log(webAppUrl);
-  var url = TELEGRAM_API_URL + "/setWebhook?url=" + webAppUrl;
+  var url = getTelegramApiUrl() + "/setWebhook?url=" + webAppUrl;
   console.log(url);
   var response = UrlFetchApp.fetch(url);
   Logger.log(response.getContentText());
+}
+
+// Helper para establecer el token desde el editor de Apps Script (run una vez)
+function setTelegramToken(token){
+  PropertiesService.getScriptProperties().setProperty('TELEGRAM_BOT_TOKEN', token);
+  Logger.log('TELEGRAM_BOT_TOKEN set');
 }
